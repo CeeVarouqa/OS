0cc6a887556795e02b7d0ad4a1d3aa13429fa6d0
CeeVarouqa <42468193+CeeVarouqa@users.noreply.github.com>
2019-09-19 20:51:38 +0300

    Week 4 lab

---
+++ week4/ex3.c
@@ -0,0 +1,18 @@
+//Created by CeeVarouqa
+#include <stdio.h>
+
+void read(char *st) 
+{
+	scanf("%s", st);
+}
+
+int main() 
+{
+	char array[100];
+	while (1) 
+    {
+		printf("$ ");
+		read(array);
+		system(array);
+	}
+}
---
+++ week4/ex4.c
@@ -0,0 +1,18 @@
+//Created by CeeVarouqa
+#include <stdio.h>
+
+void read(char *st) 
+{
+	gets(st); //reading all string until \n
+}
+
+int main() 
+{
+	char array[100];
+	while (1) 
+    {
+		printf("$ ");
+		read(array);
+		system(array);
+	}
+}
